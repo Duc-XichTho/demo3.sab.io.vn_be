dcce90248c5445db19cfd01387af485035aa327a
Duc-XichTho <ducba@xichtho-vn.com>
2025-06-16 12:29:51 +0700

    okok

--- src/routes/public/n8nWebhookSender.js
+++ src/routes/public/n8nWebhookSender.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import FormData from "form-data";
 
 const router = express.Router();
 
@@ -14,6 +15,32 @@ router.post("/send-to-n8n", async (req, res) => {
     });
     console.log(response);
     res.status(200).json({ success: true, n8nResponse: response.data });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      n8nError: error.response?.data || null
+    });
+  }
+});
+router.post("/send-to-n8n-v2", async (req, res) => {
+  // const n8nUrl = "https://n8n.sab.io.vn/webhook-test/download-private-drive-file";
+  const n8nUrl = "http://localhost:5678/webhook/download-private-drive-file";
+  const data = req.body;
+  try {
+    const response = await axios.post(n8nUrl, data, {
+      headers: { "Content-Type": "application/json" },
+      responseType: "arraybuffer"
+    });
+    const base64Data = Buffer.from(response.data, "binary").toString("base64");
+    res.status(200).json({
+      success: true,
+      n8nResponse: {
+        data: base64Data,
+        status: response.status,
+        headers: response.headers
+      }
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
